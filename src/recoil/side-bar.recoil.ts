f3c35e70ef03cda8cca97807ae9afb611add2c22
padi-dev-hoantm <124114712+padi-dev-hoantm@users.noreply.github.com>
2023-03-15 15:38:55 +0700

    Add recoid state current menu

--- src/recoil/side-bar.recoil.ts
+++ src/recoil/side-bar.recoil.ts
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { atom, useRecoilValue } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist()
 
-export const currentMenuState = atom({
+export const currentMenuItemState = atom({
     key: 'currentMenuItem',
     default: '',
     effects_UNSTABLE: [persistAtom]
 })
+
+export function useCurrentMenuItemState(){
+    const [isInitial, setIsInitial] = useState(true);
+    const currentMenuItem = useRecoilValue(currentMenuItemState);
+
+    useEffect(()=>{
+        setIsInitial(false)
+    },[])
+
+    return isInitial ? '' : currentMenuItem;
+}
